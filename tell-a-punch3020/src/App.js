9e7ac2b2d6532bbf868b66a467db462ba3f8f715
rhinehardtc <rhinehardtc@gmail.com>
2020-10-12 22:33:17 -0500

    basic responses

--- tell-a-punch3020/src/App.js
+++ tell-a-punch3020/src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+export default class App extends React.Component {
+
+  constructor(){
+    super()
+    this.state ={
+
+    }
+  }
+
+  keyLogger = (event) => {
+    console.log(event.key)
+  }
+
+  connectedController = 
+    window.addEventListener("gamepadconnected", function(e) {
+      console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
+        e.gamepad.index, e.gamepad.id,
+        e.gamepad.buttons.length, e.gamepad.axes.length);
+  });
+
+  disconnectedController = 
+    window.addEventListener("gamepaddisconnected", function(e) {
+      console.log("Gamepad disconnected from index %d: %s",
+        e.gamepad.index, e.gamepad.id);
+  });
+
+  keyPress = 
+    window.addEventListener('keydown', (e) => this.keyLogger(e));
+
+  bundle = (
+    this.connectedController,
+    this.disconnectedController,
+    this.keyPress
+  )
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header" onFocus={this.bundle}>
+        </header>
+      </div>
+    )
+  }
+}
