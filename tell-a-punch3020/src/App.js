f2878fdd2c25754b87f0f0cbe7b76e8ff3fef5d2
J-Fisk87 <Jeff.Fisk512@gmail.com>
2020-10-15 15:04:49 -0500

    implemented controller input for P1
    built a faux event listener for gamePad[0] that translates button indices into screen outputs via switch

--- tell-a-punch3020/src/App.js
+++ tell-a-punch3020/src/App.js
@@ -12,29 +12,61 @@ export default class App extends React.Component {
       comboArray2: [],
       gamePad: {
         id: "no gamepad connected",
-        buttons: [
-          {pressed: "no gamepad, no buttons"}
-        ]
-      }
+        buttons: [{ pressed: "no gamepad, no buttons" }],
+      },
     };
   }
 
   keyLogger = (event) => {
     console.log(event.key);
-    const p1Keys = {'1': 'A','2': 'B','3': 'X','4': 'Y','q': '▼','w': '▶︎','e': '◀︎','r': '▲'}
-    const p2Keys = {'7': 'A','8': 'B','9': 'X','0': 'Y','u': '▼','i': '▶︎','o': '◀︎','p': '▲'}
-    const displayedInputs = ['A','B','X','Y','▼','▶︎','◀︎','▲']
+    const p1Keys = {
+      1: "A",
+      2: "B",
+      3: "X",
+      4: "Y",
+      q: "▼",
+      w: "▶︎",
+      e: "◀︎",
+      r: "▲",
+    };
+    const p2Keys = {
+      7: "A",
+      8: "B",
+      9: "X",
+      0: "Y",
+      u: "▼",
+      i: "▶︎",
+      o: "◀︎",
+      p: "▲",
+    };
+    const displayedInputs = ["A", "B", "X", "Y", "▼", "▶︎", "◀︎", "▲"];
 
-    if (p1Keys[event.key] && this.state.comboArray1.length !== this.state.maxLength){
-      this.setState({ comboArray1: [...this.state.comboArray1, p1Keys[event.key]] })
-    } else if (p1Keys[event.key] && this.state.comboArray1.length === this.state.maxLength) {
-      this.setState({ comboArray1: [] })
+    if (
+      p1Keys[event.key] &&
+      this.state.comboArray1.length !== this.state.maxLength
+    ) {
+      this.setState({
+        comboArray1: [...this.state.comboArray1, p1Keys[event.key]],
+      });
+    } else if (
+      p1Keys[event.key] &&
+      this.state.comboArray1.length === this.state.maxLength
+    ) {
+      this.setState({ comboArray1: [] });
     }
 
-    if (p2Keys[event.key] && this.state.comboArray2.length !== this.state.maxLength){
-      this.setState({ comboArray2: [...this.state.comboArray2, p2Keys[event.key]] })
-    } else if (p2Keys[event.key] && this.state.comboArray2.length === this.state.maxLength) {
-      this.setState({ comboArray2: [] })
+    if (
+      p2Keys[event.key] &&
+      this.state.comboArray2.length !== this.state.maxLength
+    ) {
+      this.setState({
+        comboArray2: [...this.state.comboArray2, p2Keys[event.key]],
+      });
+    } else if (
+      p2Keys[event.key] &&
+      this.state.comboArray2.length === this.state.maxLength
+    ) {
+      this.setState({ comboArray2: [] });
     }
   };
 
@@ -50,7 +82,7 @@ export default class App extends React.Component {
 
   disconnectedController = window.addEventListener(
     "gamepaddisconnected",
-    function (e) {
+    (e) => {
       console.log(
         "Gamepad disconnected from index %d: %s",
         e.gamepad.index,
@@ -62,14 +94,49 @@ export default class App extends React.Component {
   keyPress = window.addEventListener("keydown", (e) => this.keyLogger(e));
 
   update = () => {
-    const gamepads = navigator.getGamepads()
+    const gamepads = navigator.getGamepads();
 
-    gamepads[0] 
-    ? this.setState({gamePad: gamepads[0]}) 
-    : this.setState(prevState => ({gamePad: {...prevState.gamePad, id: 'no gamepad connected'}}))
+    if (gamepads[0]) {
+      this.setState({ gamePad: gamepads[0] });
 
-    window.requestAnimationFrame(this.update)
-  }
+      this.state.gamePad.buttons.forEach((button) => {
+        if (button.pressed) {
+          switch (this.state.gamePad.buttons.indexOf(button)) {
+            case 0:
+              this.keyLogger({ key: "1" });
+              break;
+            case 1:
+              this.keyLogger({ key: "2" });
+              break;
+            case 2:
+              this.keyLogger({ key: "3" });
+              break;
+            case 3:
+              this.keyLogger({ key: "4" });
+              break;
+            case 4:
+              this.keyLogger({ key: "q" });
+              break;
+            case 5:
+              this.keyLogger({ key: "w" });
+              break;
+            case 6:
+              this.keyLogger({ key: "e" });
+              break;
+            case 7:
+              this.keyLogger({ key: "r" });
+          }
+        }
+      });
+
+    } else {
+      this.setState((prevState) => ({
+        gamePad: { ...prevState.gamePad, id: "no P1 gamepad connected" },
+      }));
+    }
+
+    window.requestAnimationFrame(this.update);
+  };
 
   runUpdate = window.requestAnimationFrame(this.update);
 
@@ -77,22 +144,34 @@ export default class App extends React.Component {
     return (
       <div className="App">
         <div className="p1_button_list">
-            <ol>
-              {this.state.gamePad.buttons 
-               ? this.state.gamePad.buttons.map(button => <li key={this.state.gamePad.buttons.indexOf(button)}>{String(button.pressed)}</li>) 
-               : <li>"no gamepad, no buttons"</li>}
-            </ol>
+          <ol>
+            {this.state.gamePad.buttons ? (
+              this.state.gamePad.buttons.map((button) => (
+                <li key={this.state.gamePad.buttons.indexOf(button)}>
+                  {String(button.pressed)}
+                </li>
+              ))
+            ) : (
+              <li>"no gamepad, no buttons"</li>
+            )}
+          </ol>
         </div>
         <div className="p2_button_list">
-            <ol>
-              {this.state.gamePad.buttons 
-               ? this.state.gamePad.buttons.map(button => <li key={this.state.gamePad.buttons.indexOf(button)}>{String(button.pressed)}</li>) 
-               : <li>"no gamepad, no buttons"</li>}
-            </ol>
+          <ol>
+            {this.state.gamePad.buttons ? (
+              this.state.gamePad.buttons.map((button) => (
+                <li key={this.state.gamePad.buttons.indexOf(button)}>
+                  {String(button.pressed)}
+                </li>
+              ))
+            ) : (
+              <li>"no gamepad, no buttons"</li>
+            )}
+          </ol>
         </div>
         <header className="App-header">
-          <ComboConsole combo={this.state.comboArray1} p1={true}/>
-          <ComboConsole combo={this.state.comboArray2} p2={true}/>
+          <ComboConsole combo={this.state.comboArray1} p1={true} />
+          <ComboConsole combo={this.state.comboArray2} p2={true} />
         </header>
         <h4 className="gamepad_display">{this.state.gamePad.id}</h4>
       </div>
