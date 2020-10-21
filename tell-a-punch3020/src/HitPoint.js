f90ca642df2cbb21c96f3ead013986d31133125d
rhinehardtc <rhinehardtc@gmail.com>
2020-10-20 22:47:48 -0500

    Health bar functions correctly and is styled. Need to add time bar/slider.

--- tell-a-punch3020/src/HitPoint.js
+++ tell-a-punch3020/src/HitPoint.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
 const HitPoint = (props) => {
-    const {hitPoint} = props
+    const {hitPoint} = props;
+    const slot = ' ';
+
+    const slotFill = () => {
+        //Both filled and unfilled hitpoints are just single spaces, but filled hitpoints are styled to have background color.
+        //Create a filled hitpoint id the value passed into props is 1, otherwise create an unfilled hitpoint.
+        return hitPoint === 1 ? <span className="hitpoint filled_hitpoint">{slot}</span> : <span className="hitpoint unfilled_hitpoint">{slot}</span>
+    }
 
     return (
-        <span className="hitpoint">{hitPoint}</span>
+        <>
+            {slotFill()}
+        </>
     )
 }
 
