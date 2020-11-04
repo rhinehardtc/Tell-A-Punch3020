543d9eed5d64d87c75eddc8c7c6d216f59664fb0
rhinehardtc <rhinehardtc@gmail.com>
2020-11-03 22:48:13 -0600

    Updates

---
+++ precache-manifest.187e4464f34dd3021809e66f01b7656d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb6020e27b1a279bc77cbd7bb0c3704e",
+    "url": "/Tell-A-Punch3020/index.html"
+  },
+  {
+    "revision": "a910caaaa90035c75d03",
+    "url": "/Tell-A-Punch3020/static/css/main.20e3fa59.chunk.css"
+  },
+  {
+    "revision": "8fed4c07c1b8720c4526",
+    "url": "/Tell-A-Punch3020/static/js/2.ae30fb48.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/Tell-A-Punch3020/static/js/2.ae30fb48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a910caaaa90035c75d03",
+    "url": "/Tell-A-Punch3020/static/js/main.7d2d289f.chunk.js"
+  },
+  {
+    "revision": "e0be986b3f93e00e80b3",
+    "url": "/Tell-A-Punch3020/static/js/runtime-main.d6faefd9.js"
+  }
+]);
